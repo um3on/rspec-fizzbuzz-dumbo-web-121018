ec0c94fb255a8c9cdf9f910985ad637b82df355a
M. T <mariam.tamimi@gmail.com>
2018-11-26 03:33:33 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,7 +4,7 @@ def fizzbuzz(num)
   if(num%3 == 0 && num%5 != 0)
     return "Fizz"
   elsif (num%3 !=0 && num%5 ==0)
-    print "Buzz"
+    return "Buzz"
   elsif(num%3 ==0 && num%5 ==0)
     print "FizzBuzz"
   else
